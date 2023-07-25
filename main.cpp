06091a59111f4198cee6f5efcd683623a4e5a7a1
Vitalii Shkibtan <vetalguru@gmail.com>
2023-07-25 11:59:45 +0300

    Output unprintable symbols

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ bool ReadData(const std::string& aPath, std::vector<unsigned char>& aData)
     }
 
     // Open file
-    std::fstream file;
+    std::ifstream file;
     file.open(aPath, std::ios_base::in | std::ios_base::binary);
     if (!file.is_open())
     {
@@ -40,10 +40,46 @@ void PrintHEX(const std::vector<unsigned char>& aData)
 {
     for (const auto& i : aData)
     {
-        std::cerr << (int)i;
+        switch(static_cast<uint8_t>(i))
+        {
+            case 0:   { std::cout << ".";     break; }  // null character
+            case 1:   { std::cout << "[SOH]"; break; }  // start of header
+            case 2:   { std::cout << "[STX]"; break; }  // start of text
+            case 3:   { std::cout << "[ETX]"; break; }  // end of text
+            case 4:   { std::cout << "[EOT]"; break; }  // end of transmission
+            case 5:   { std::cout << "[ENQ]"; break; }  // enquiry
+            case 6:   { std::cout << "[ACK]"; break; }  // acknowledge
+            case 7:   { std::cout << "[BEL]"; break; }  // bell (ring)
+            case 8:   { std::cout << "[BS]";  break; }  // backspace
+            case 9:   { std::cout << "[HT]";  break; }  // horizontal tab
+            case 10:  { std::cout << "[LF]";  break; }  // line feed
+            case 11:  { std::cout << "[VT]";  break; }  // vertical tab
+            case 12:  { std::cout << "[FF]";  break; }  // form feed
+            case 13:  { std::cout << "[CR]";  break; }  // carriage return
+            case 14:  { std::cout << "[SO]";  break; }  // shift out
+            case 15:  { std::cout << "[SI]";  break; }  // shift in
+            case 16:  { std::cout << "[DLE]"; break; }  // data link escape
+            case 17:  { std::cout << "[DC1]"; break; }  // device control 1
+            case 18:  { std::cout << "[DC2]"; break; }  // device control 2
+            case 19:  { std::cout << "[DC3]"; break; }  // device control 3
+            case 20:  { std::cout << "[DC4]"; break; }  // device control 4
+            case 21:  { std::cout << "[NAK]"; break; }  // negative acknowledge
+            case 22:  { std::cout << "[SYN]"; break; }  // synchronize
+            case 23:  { std::cout << "[ETB]"; break; }  // end transmission block
+            case 24:  { std::cout << "[CAN]"; break; }  // cancel
+            case 25:  { std::cout << "[EM]";  break; }  // end of medium
+            case 26:  { std::cout << "[SUB]"; break; }  // substitute
+            case 27:  { std::cout << "[ESC]"; break; }  // escape
+            case 28:  { std::cout << "[FS]";  break; }  // file separator
+            case 29:  { std::cout << "[GS]";  break; }  // group separator
+            case 30:  { std::cout << "[RS]";  break; }  // record separator
+            case 31:  { std::cout << "[US]";  break; }  // unit separator
+            case 127: { std::cout << "[DEL]"; break; }  // delete (rubout)
+            default:  { std::cout << i;       break; }
+        }
     }
 
-    std::cerr << "\n";
+    std::cout << "\n";
 }
 
 bool PrintElfHeader(const std::vector<unsigned char>& aData)
@@ -98,7 +134,7 @@ int main ()
     std::vector<unsigned char> data;
     if (ReadData(std::string{"samples/simple.bin"}, data))
     {
-        //PrintHEX(data);
+        PrintHEX(data);
         PrintElfHeader(data);
     }
 
