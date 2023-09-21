5f592e63184a4f3f659fc93f33ff087372288bb2
Vitalii <vetalguru@gmail.com>
2023-09-21 11:00:16 +0300

    Move logic to main file

--- lib/inc/elfdecoder.h
+++ lib/inc/elfdecoder.h
@@ -1,13 +1,5 @@
 #ifndef ELFDECODER_H
 #define ELFDECODER_H
 
-#include <string>
-#include <vector>
-
-bool ReadData(const std::string& aPath, std::vector<unsigned char>& aData);
-
-void PrintHEX(const std::vector<unsigned char>& aData);
-
-bool PrintElfHeader(const std::vector<unsigned char>& aData);
 
 #endif // ELFDECODER_H
